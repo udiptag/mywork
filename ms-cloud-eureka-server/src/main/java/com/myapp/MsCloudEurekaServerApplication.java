e1a500647816223ccb466fafd32d952a25c066be
udiptag <udipta.aquila@gmail.com>
2017-06-29 14:14:09 +0530

    adding eureka server

---
+++ ms-cloud-eureka-server/src/main/java/com/myapp/MsCloudEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.myapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+
+public class MsCloudEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCloudEurekaServerApplication.class, args);
+	}
+}
